354f63c3efb565593f71f2ee834a3a4397f942b6
SavannahBolger <edenbolger@gmail.com>
2018-01-10 22:46:18 +0000

    variables

--- 3D Vector and Matrix Project/3D Vector and Matrix Project/Matrix3.h
+++ 3D Vector and Matrix Project/3D Vector and Matrix Project/Matrix3.h
@@ -7,13 +7,30 @@
 /// </summary>
 
 #include <iostream>
+#include "Vector3D.h"
 
 class Matrix3
 {
 public:
 	Matrix3();
+	Matrix3(double a11, double a12, double a13,
+		double a21, double a22, double a23,
+		double a31, double a32, double a33);
+	Matrix3(Vector3D row1, Vector3D row2, Vector3D row3);
 	~Matrix3();
 
-private:
+	static Matrix3 rotationZ(double angleRadians);
+	static Matrix3 rotationY(double angleRadians);
+	static Matrix3 rotationX(double angleRadians);
 
+private:
+	double m11;
+	double m12;
+	double m13;
+	double m21;
+	double m22;
+	double m23;
+	double m31;
+	double m32;
+	double m33;
 };
--- 3D Vector and Matrix Project/3D Vector and Matrix Project/Vector3D.h
+++ 3D Vector and Matrix Project/3D Vector and Matrix Project/Vector3D.h
@@ -12,8 +12,17 @@ class Vector3D
 {
 public:
 	Vector3D();
+	Vector3D(double x, double y, double z);
 	~Vector3D();
 
-private:
+	double length()const;
+	double lengthSquared()const;
+	double angleBetween(const Vector3D other)const;
+	Vector3D unit() const;
+	void normalise();
 
+private:
+	double m_x;
+	double m_y;
+	double m_z;
 };
